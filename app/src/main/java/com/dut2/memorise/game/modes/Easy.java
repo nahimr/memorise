1de87b788e7af5ddddfe07f6b7b8906ec3ce8c87
Nahim <nahim.rouidjali@gmail.com>
2021-02-23 00:19:07 +0100

    Add Easy level

---
+++ app/src/main/java/com/dut2/memorise/game/modes/Easy.java
@@ -0,0 +1,10 @@
+package com.dut2.memorise.game.modes;
+
+import com.dut2.memorise.game.Engine;
+
+public class Easy extends Engine {
+
+    public Easy() {
+        super((byte)0, (byte)3, (byte)10,  (byte)2, 1.0f, false);
+    }
+}
