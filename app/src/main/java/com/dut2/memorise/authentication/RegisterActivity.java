d9b91d181016a13d951b4fc6af0f757b7bdea767
Nahim <nahim.rouidjali@gmail.com>
2021-02-23 00:18:05 +0100

    Add Register Activity

---
+++ app/src/main/java/com/dut2/memorise/authentication/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.dut2.memorise.authentication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.dut2.memorise.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth_register);
+        final EditText email = findViewById(R.id.register_email);
+        final EditText username = findViewById(R.id.register_username);
+        final EditText password = findViewById(R.id.register_password);
+        final Button loginButton = findViewById(R.id.loginButton);
+        final Button registerButton = findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(v -> {
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            finish();
+        });
+    }
+}
