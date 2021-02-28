7f1f330fd327fd9a6db6ca91b7187861fb84dea0
Nahim <nahim.rouidjali@gmail.com>
2021-02-28 17:06:35 +0100

    Add block thread to manage lighting

---
+++ app/src/main/java/com/dut2/memorise/game/thread/BlockThread.java
@@ -0,0 +1,26 @@
+package com.dut2.memorise.game.thread;
+
+import com.dut2.memorise.game.view.Block;
+
+public class BlockThread extends Thread{
+    // Make Thread Suspendable !
+    private final long TIMER;
+    private final Block block;
+
+    public BlockThread(Block block, long TIMER){
+        this.TIMER = TIMER;
+        this.block = block;
+    }
+
+    @Override
+    public void run() {
+        try {
+            block.setPressed(true);
+            Thread.sleep(TIMER);
+            block.setPressed(false);
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
