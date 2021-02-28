3e9658cd57ad06d6e98d450eef3bac8df9106427
Nahim <nahim.rouidjali@gmail.com>
2021-02-28 17:22:06 +0100

    Add Timer mode

--- app/src/main/java/com/dut2/memorise/game/modes/Timer.java
+++ app/src/main/java/com/dut2/memorise/game/modes/Timer.java
@@ -3,7 +3,7 @@ package com.dut2.memorise.game.modes;
 import com.dut2.memorise.game.Engine;
 
 public class Timer extends Engine {
-    public Timer(byte minLightenBlock, byte maxLives, float weight, boolean timer) {
+    public Timer() {
         super((byte) 1,(byte) 3,2.0f,true);
     }
 
