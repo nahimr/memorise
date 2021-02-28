55860b054ecd4afae24e2df2f6f7083864b5987a
Nahim <nahim.rouidjali@gmail.com>
2021-02-28 17:22:18 +0100

    Update Easy mode init

--- app/src/main/java/com/dut2/memorise/game/modes/Easy.java
+++ app/src/main/java/com/dut2/memorise/game/modes/Easy.java
@@ -5,6 +5,6 @@ import com.dut2.memorise.game.Engine;
 public class Easy extends Engine {
 
     public Easy() {
-        super((byte)0, (byte)3, (byte)10,  (byte)2, 1.0f, false);
+        super((byte)1, (byte)10,  (byte)2, 1.0f, false);
     }
 }
