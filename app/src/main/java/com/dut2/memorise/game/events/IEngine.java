7ccbb9e597cf6c6cfcc47ce7f2cd5a46bfae8c95
Nahim <nahim.rouidjali@gmail.com>
2021-03-23 18:41:41 +0100

    Update IEngine interface

--- app/src/main/java/com/dut2/memorise/game/events/IEngine.java
+++ app/src/main/java/com/dut2/memorise/game/events/IEngine.java
@@ -1,11 +1,12 @@
 package com.dut2.memorise.game.events;
 
 public interface IEngine {
-    void onInitLevel();
+    void onStartLevel();
     void onLoadBlock(byte pos);
     void onLoadLightenBlock(byte pos);
     long onBeforeLevelStart();
-    void onStartLevel();
+    void onStartPattern();
+    void onEndPattern(boolean patternWon);
     Runnable onLightenBlocksFinished();
     void onEndLevel(boolean levelWon, byte levelCount);
     void onEndGame(boolean gameWon, float points);
